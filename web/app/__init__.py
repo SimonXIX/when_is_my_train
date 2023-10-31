47febe1250a5315185a0784108fe9edcab3d280e
Simon <ad7588@coventry.ac.uk>
2023-10-31 16:06:29 +0000

    adding page for longer journeys

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -7,10 +7,9 @@
 
 from flask import Flask, render_template, request
 from flask_moment import Moment
-import requests
-import urllib.request, json
+from .rtt import *
 import os
-import base64
+import json
 
 # initiate Moment for datetime functions
 moment = Moment()
@@ -21,33 +20,19 @@ moment.init_app(app)
 
 @app.route('/')
 def index():
-    rtt_url = os.environ.get('RTT_URL')
-    rtt_username = os.environ.get('API_USERNAME')
-    rtt_password = os.environ.get('API_PASSWORD')
-
-    auth = rtt_username + ":" + rtt_password
-    auth_bytes = auth.encode("ascii")
-    base64_bytes = base64.b64encode(auth_bytes)
-    base64_string = base64_bytes.decode("ascii")
-
-    payload={}
-    headers = {"Authorization": "Basic " + base64_string}
-
     to_station = os.environ.get('TO_STATION')
     from_stations = json.loads(os.environ.get('FROM_STATIONS'))
 
-    results = []
-
-    for station in from_stations:
-        endpoint_url = rtt_url + 'json/search/' + station + '/to/' + to_station
-        response = requests.request("GET", endpoint_url, headers=headers, data=payload)
-
-        if response.status_code == 200:
-            # turn the API response into useful Json
-            response = response.json()
-            for service in response['services']:
-                results.append(service)
-
-    results.sort(key=lambda x: x['locationDetail']['realtimeArrival'])
+    results = rtt.get_arrivals(to_station, from_stations)
 
     return render_template('index.html', results=results)
+
+@app.route('/coventry')
+def coventry():
+    to_station = 'COV'
+    from_station = 'GLC'
+
+    outbound_results = rtt.get_departures(to_station, from_station)
+    return_results = rtt.get_departures(from_station, to_station)
+
+    return render_template('departures.html', outbound_results=outbound_results, return_results=return_results)
---
+++ web/app/rtt.py
@@ -0,0 +1,56 @@
+# @name: rtt.py
+# @creation_date: 2023-02-24
+# @license: The MIT License <https://opensource.org/licenses/MIT>
+# @author: Simon Bowie <ad7588@coventry.ac.uk>
+# @purpose: functions for retrieving data from the RealTimeTrains API
+# @acknowledgements:
+
+import os
+import base64
+import requests
+import urllib.request, json
+
+rtt_url = os.environ.get('RTT_URL')
+rtt_username = os.environ.get('API_USERNAME')
+rtt_password = os.environ.get('API_PASSWORD')
+
+auth = rtt_username + ":" + rtt_password
+auth_bytes = auth.encode("ascii")
+base64_bytes = base64.b64encode(auth_bytes)
+base64_string = base64_bytes.decode("ascii")
+
+payload={}
+headers = {"Authorization": "Basic " + base64_string}
+
+def get_arrivals(to_station, from_stations):
+    results = []
+
+    for station in from_stations:
+        endpoint_url = rtt_url + 'json/search/' + station + '/to/' + to_station
+        response = requests.request("GET", endpoint_url, headers=headers, data=payload)
+
+        if response.status_code == 200:
+            # turn the API response into useful Json
+            response = response.json()
+            for service in response['services']:
+                results.append(service)
+
+    results.sort(key=lambda x: x['locationDetail']['realtimeArrival'])
+
+    return results
+
+def get_departures(to_station, from_station):
+    results = []
+    endpoint_url = rtt_url + 'json/search/' + from_station + '/to/' + to_station
+    response = requests.request("GET", endpoint_url, headers=headers, data=payload)
+
+    if response.status_code == 200:
+        # turn the API response into useful Json
+        response = response.json()
+        if response['services'] is not None:
+            for service in response['services']:
+                results.append(service)
+        else:
+            results = 'None'
+
+    return results
